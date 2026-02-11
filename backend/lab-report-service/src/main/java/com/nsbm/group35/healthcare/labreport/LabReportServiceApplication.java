248364cb415b21d56a5164d2c82be3bdbbe837ef
Chamith Harshamal <chamithweerasinghe1@gmail.com>
2026-02-11 20:57:43 +0530

    feat: scaffold services and clean configs

---
+++ backend/lab-report-service/src/main/java/com/nsbm/group35/healthcare/labreport/LabReportServiceApplication.java
@@ -0,0 +1,12 @@
+package com.nsbm.group35.healthcare.labreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabReportServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabReportServiceApplication.class, args);
+    }
+}
---
+++ backend/staff-service/src/main/java/com/nsbm/group35/healthcare/staff/StaffServiceApplication.java
@@ -0,0 +1,12 @@
+package com.nsbm.group35.healthcare.staff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaffServiceApplication.class, args);
+    }
+}
